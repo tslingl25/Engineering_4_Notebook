8ced8983294adeeb4415213593d66514442eb0d3
tslingl25 <tslingl25@charlottesvilleschools.org>
2022-10-11 13:41:28 -0400

    dfzshb

--- raspberry-pi/crashaviodence.py
+++ raspberry-pi/crashaviodence.py
@@ -3,7 +3,10 @@ import adafruit_mpu6050
 import busio
 import board
 import time
+import digitalio
 
+led = digitalio.DigitalInOut(board.GP0)
+led.direction = digitalio.Direction.OUTPUT
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
@@ -13,4 +16,7 @@ mpu.acceleration
 while True:
     time.sleep(.2)
     print(mpu.acceleration)
-
+    if(mpu.acceleration[0] > 8) or (mpu.acceleration[2] < 3):
+     led.value = True
+    else:
+        led.value = False
--- raspberry-pi/upload-test.py
+++ raspberry-pi/upload-test.py
@@ -1,1 +1,22 @@
-#yo this a test
+import board
+import time
+import busio
+
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
+
+while not i2c.try_lock():
+    pass
+
+try:
+    while True:
+        print(
+            "I2C addresses found:",
+            [hex(device_address) for device_address in i2c.scan()],
+        )
+        time.sleep(2)
+
+finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
+    i2c.unlock()
